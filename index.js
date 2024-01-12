ce2da373e20fe488c41f5f683f65b265c3d382e5
Simon Cheek <simon.cheek@yahoo.com>
2024-01-11 19:59:43 -0700

    add create functionality and routing

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-
-const comments = [
-    {
-        user: 'Simon593462',
-        comment: 'This thing is so cool! Wow, look at this!'
-    }, {
-        user: 'KaylaWestViolist',
-        comment: 'This is so wack.'
-    }
-]
-
-
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const comments = [
+    {
+        user: 'User1',
+        comment: 'This thing is so cool! Wow, look at this!'
+    }, {
+        user: 'User2',
+        comment: 'Nice!'
+    }
+]
 
 
 
@@ -27,9 +27,20 @@ app.get('/', (req, res) => {
     res.render('home.ejs', { comments });
 });
 
+
 app.get('/new', (req, res) => {
     res.render('new.ejs');
 });
+
+
+app.post('/', (req, res) => {
+    console.log('it works!');
+    const { username: user, content: comment } = req.body;
+    console.log(req.body);
+    comments.push({ user, comment });
+    console.log(comments);
+    res.redirect('/');
+})
 
 
 
---
+++ public/app.js
@@ -0,0 +1,12 @@
+const createForm = document.querySelector('#createForm');
+let createUser = document.querySelector('#createUser');
+let createContent = document.querySelector('#createContent');
+
+
+
+createForm.addEventListener('submit', (e) => {
+    if ((!createUser.value) || (!createContent.value)) {
+        e.preventDefault();
+        alert('Please enter a username and comment!');
+    }
+})
