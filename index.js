2cb51b619dc823f43de7b37180085ebb48c1c7b3
Simon Cheek <simon.cheek@yahoo.com>
2024-01-19 17:45:47 -0700

    spend 3 hours debugging GET requests errors to only find that I needed an extra slash in static file paths

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = 3000;
 const path = require('path');
 const methodOverride = require('method-override');
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -14,11 +14,11 @@ app.use(methodOverride('_method'));
 
 let comments = [
     {
-        id: 0,
+        _id: 0,
         user: 'User1',
         comment: 'This thing is so cool! Wow, look at this!'
     }, {
-        id: 1,
+        _id: 1,
         user: 'User2',
         comment: 'Nice!'
     }
@@ -36,23 +36,28 @@ app.get('/new', (req, res) => {
     res.render('new.ejs');
 });
 
-app.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const comment = comments.find(com => com.id === parseInt(id));
-    res.render('edit.ejs', { comment });
+app.get('/comment/:_id', (req, res) => {
+    const { _id } = req.params;
+    const comment = comments.find(com => com._id === parseInt(_id));
+    if (!comment) {
+        console.log('oops');
+        res.redirect('/');
+    } else {
+        res.render('edit.ejs', { comment });
+    }
 });
 
-app.patch('/:id', (req, res) => {
-    const { id } = req.params;
+app.patch('/comment/:_id', (req, res) => {
+    const { _id } = req.params;
     const newText = req.body.commentEdit;
-    const comment = comments.find(com => com.id === parseInt(id));
+    const comment = comments.find(com => com._id === parseInt(_id));
     comment.comment = newText;
     res.redirect('/');
 });
 
-app.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    comments = comments.filter(c => c.id != id);
+app.delete('/comment/:_id', (req, res) => {
+    const { _id } = req.params;
+    comments = comments.filter(c => c._id != _id);
     res.redirect('/');
 });
 
@@ -60,7 +65,7 @@ app.delete('/:id', (req, res) => {
 app.post('/', (req, res) => {
     const { username: user, content: comment } = req.body;
     let newId = comments.length;
-    comments.push({ id: newId, user, comment, });
+    comments.push({ _id: newId, user, comment, });
     res.redirect('/');
 })
 
